0bfbc19a4885dc0bd7d3836e27d8a69eee12d42a
dpq96 <mathias.kranz@uni-ulm.de>
2026-01-29 15:06:20 +0100

    Added end to end test for document upload, chat and report generation

--- tests/integration/test_chat_page.py
+++ tests/integration/test_chat_page.py
@@ -3,15 +3,24 @@ from playwright.sync_api import expect
 
 BASE_URL = os.getenv("BASE_URL", "http://localhost:8501")
 
+def test_chat_page_title(page):
+    """Test the Title of the Chat page"""
+    page.goto(BASE_URL)
+
+    page.wait_for_load_state("networkidle")
+
+    assert "Digital Assistant - RAG-LLM" in page.title()
+
 def test_chat_page_heading(page):
     """Chat page displays correct heading"""
     page.goto(BASE_URL)
+
+    page.wait_for_load_state("networkidle")
 
     heading = page.get_by_role(
         "heading",
         name="🤖 Digital Assistant - RAG-LLM (Hybrid)"
     )
-    heading.wait_for(state="visible")
 
     expect(heading).to_be_visible()
     expect(heading).to_have_text("🤖 Digital Assistant - RAG-LLM (Hybrid)")
@@ -20,8 +29,7 @@ def test_chat_page_has_chat_input(page):
     """Chat page contains chat input field"""
     page.goto(BASE_URL)
 
-    # wait for page to load, more determinstic solution than using networkidle
-    page.get_by_role("heading", name="🤖 Digital Assistant - RAG-LLM (Hybrid)").wait_for(state="visible")
+    page.wait_for_load_state("networkidle")
 
     chat_input = page.get_by_test_id("stChatInputTextArea")
 
@@ -32,8 +40,7 @@ def test_chat_page_has_login_page(page):
     """Chat page has navigation to user login page"""
     page.goto(BASE_URL)
 
-    # wait for page to load, more determinstic solution than using networkidle
-    page.get_by_role("heading", name="🤖 Digital Assistant - RAG-LLM (Hybrid)").wait_for(state="visible")
+    page.wait_for_load_state("networkidle")
 
     expect(page.get_by_test_id("stToolbar").get_by_text("User")).to_be_visible()
 
